afa2dfde5f8411480c3859ad34ef5f60c0dd7f1d
Bernal Araya <bernala@thehangar.cr>
2017-05-07 20:53:34 -0600

    user controler ready

---
+++ controlers/user-controler.php
@@ -0,0 +1,90 @@
+<?php
+include ("../model/user-model.php");
+
+// Select all users 
+function getUsers(){
+	$result = selectUsers();//funcition from user-model
+	// echo gettype($result);
+	if (gettype($result)!= 'string') {; //cheking type of data.
+		$rows = array();
+		while($r = $result->fetch_assoc()) {
+		    $rows[] = $r;
+		}
+		return json_encode($rows);
+	}
+	return $result;	
+}
+
+// Select specific User by Id
+function getSpecificUser($userId){
+	$result = selectSpecificUser($userId);//funcition from user-model
+	// echo gettype($result);
+	if (gettype($result)!= 'string') {; //cheking type of data.
+		$rows = array();
+		while($r = $result->fetch_assoc()) {
+		    $rows[] = $r;
+		}
+		return json_encode($rows);//convertir a json
+	}
+	return $result;	
+}
+
+
+// Select specific User By Employee Number
+function getUserByNumber($employeeNumber){
+	$result = selectUserByNumber($employeeNumber);//funcition from user-model
+	// echo gettype($result);
+	if (gettype($result)!= 'string') {; //cheking type of data.
+		$rows = array();
+		while($r = $result->fetch_assoc()) {
+		    $rows[] = $r;
+		}
+		return json_encode($rows);//convertir a json
+	}
+	return $result;	
+}
+
+//Add User
+function newUser($employeeNumber, $name, $email, $department, $type){
+	$result = getUserByNumber($employeeNumber);
+	echo "--------";
+	print $result;
+	echo "--------";
+	print gettype($result);
+	if ($result == '0 results'){
+		$result = addUser($employeeNumber, $name, $email, $department, $type);//funcition from user-model
+		return "<br>User Added<br>";
+	}
+	return "<br>Employee Number already used<br>";
+}
+
+
+//Update user
+function modifyUser($id, $employeeNumber, $name, $email, $department, $type){
+	$result = updateUser($id, $employeeNumber, $name, $email, $department, $type);
+	echo "--------";
+	print $result;
+	echo "--------";
+}
+
+//Delete user
+function deleteUser($id){
+	$result = deleteUserById($id);
+	echo "--------";
+	print $result;
+	echo "--------";
+}
+
+
+
+print deleteUser(1);
+print getUsers();
+// print getUserByNumber(320);
+
+
+//addUser(320, "Bernal Araya" , "bernala@gmail.com",2);
+// $result = selectUsers();
+// while($row = $result->fetch_assoc()) {
+//         echo "<br>- id: " . $row["idUser"]. " - EmpNum: " . $row["EmployeeNumber"] . " - Name: " . $row["Name"]. " - Email " . $row["Email"]. "<br>";
+// }
+?>	
--- model/user-model.php
+++ model/user-model.php
@@ -13,11 +13,11 @@ function selectUsers(){
 	return $result;
 }
 
-// Select specific User
+// Select specific User by Id
 function selectSpecificUser($userId){
 	global $conn;
 	$sql = "SELECT * FROM User where idUser = " . $userId;
-	echo"<br>Select from selectSpecificUser: "  . $sql;
+	echo"<br>Select from selectSpecificUser: "  . $sql . "<br>";
 	$result = $conn->query($sql);
 	if ($result->num_rows == 0){
 		return "0 results";
@@ -25,45 +25,59 @@ function selectSpecificUser($userId){
 	return $result;
 }
 
-// Update User
-function updateUser($id, $employeeNumber, $name, $email, $type){
+// Select specific User By Employee Number
+function selectUserByNumber($employeeNumber){
 	global $conn;
-	$sql = "UPDATE `User` SET `EmployeeNumber` = \"" . $employeeNumber ."\" , `Name` = \"" . $name . "\" , `Email` = \"" . $email . "\" , `UserTypeId` = \"" . $type . "\" WHERE `idUser`=  \"" . $id . "\"";
-	echo"<br>Select from selectSpecificUser=" . $sql;
+	$sql = "SELECT * FROM User where EmployeeNumber = " . $employeeNumber;
+	echo"<br>Select from selecUserByNumber: "  . $sql . "<br>";
+	$result = $conn->query($sql);
+	if ($result->num_rows == 0){
+		return "0 results";
+	}
+	return $result;
+}
+
+// Add User
+function addUser($employeeNumber, $name, $email, $department, $type){
+	global $conn;
+	//INSERT INTO `ExpRep_DB`.`User` (`EmployeeNumber`, `Name`, `Email`, `UserTypeId`) VALUES ('318', 'Bernal', 'bernala@thehanger.cr', '1');
+	$sql = "INSERT INTO `User` (`EmployeeNumber`, `Name`, `Email`, `Department`, `UserTypeId`) VALUES ( '" . $employeeNumber . "', '" . $name . "', '" . $email . "', '" . $department . "', '" . $type . "');";
+	echo "<br>Select from add User:" . $sql . "<br>";
 	$result = $conn->query($sql);
 	echo "<br>" . $result ."<br>";
 	return $result;
 
 }
 
+
+
+// Update User
+function updateUser($id, $employeeNumber, $name, $email, $department, $type){
+	global $conn;
+	$sql = "UPDATE `User` SET `EmployeeNumber` = \"" . $employeeNumber ."\" , `Name` = \"" . $name . "\" , `Email` = \"" . $email . "\" , `Department` = \"" . $department . "\" , `UserTypeId` = \"" . $type . "\" WHERE `idUser`=  \"" . $id . "\"";
+	echo"<br>Select from selectSpecificUser=" . $sql . "<br>";
+	$result = $conn->query($sql);
+	//echo "<br>" . $result ."<br>";
+	return $result;
+
+}
+
 //Delete User 
-function deleteUser($id){
+function deleteUserById($id){
 	global $conn;
 	$sql = "DELETE FROM User WHERE `idUser`=  \"" . $id . "\"";
-	echo"<br>Select from selectSpecificUser=" . $sql;
+	echo"<br>Select from selectSpecificUser=" . $sql . "<br>";
 	$result = $conn->query($sql);
 	return $result;
 }
 
-deleteUser(5);
-$result = selectUsers();
-while($row = $result->fetch_assoc()) {
-        echo "<br>- id: " . $row["idUser"]. " - Name: " . $row["Name"]. " - Email " . $row["Email"]. "<br>";
-}
 
-
-// $sql = "SELECT idUser, Name, Email FROM User";
-
-// $result = $conn->query($sql);
-
-// if ($result->num_rows > 0) {
-//     // output data of each row
-//     while($row = $result->fetch_assoc()) {
-//         echo "- id: " . $row["idUser"]. " - Name: " . $row["Name"]. " - Email " . $row["Email"]. "<br>";
-//     }
-// } else {
-//     echo "0 results";
+// $result = selectUsers();
+// $rows = array();
+// while($r = $result->fetch_assoc()) {
+//     $rows[] = $r;
 // }
+// print json_encode($rows);
 
 ?>
 
